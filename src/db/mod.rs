2045419bab3c40b6b89602da520aa43e3a8d1d80
juppo <rbwilkes@yahoo.com>
2025-01-09 08:59:28 -0600

    add mod to vcs

---
+++ src/db/mod.rs
@@ -0,0 +1,13 @@
+pub(crate) mod team;
+pub(crate) mod physical_score;
+pub(crate) mod mental_score;
+
+// src/db.rs
+use tokio_postgres::{NoTls, Error};
+use tokio_postgres::tls::NoTlsStream;
+
+pub async fn connect_to_db() -> Result<(tokio_postgres::Client, tokio_postgres::Connection<tokio_postgres::Socket, NoTlsStream>), Error> {
+    let (client, connection) =
+        tokio_postgres::connect("host=localhost user=postgres password=password dbname=practicedb", NoTls).await?;
+    Ok((client, connection))
+}
